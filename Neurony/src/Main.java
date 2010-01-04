3b126343c5a155a54ba0bcbd6048ef9f625b3a43
cwiku86 <cwiku86@429f14d4-f983-11de-973d-3ba8dba7af4f>
2010-01-04 22:59:28 +0000

    Sieci neuronowe

---
+++ Neurony/src/Main.java
@@ -0,0 +1,21 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Pawel
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame main = new MainFrame();
+        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main.pack();
+        main.setVisible(true);
+    }
+}
